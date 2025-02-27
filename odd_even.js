ef4282f2554797fe2b44e83c900db356fe6a0764
AnithaDayalasamy <anithad1989@gmail.com>
2025-02-27 22:09:43 +0530

    Add files via upload

---
+++ odd_even.js
@@ -0,0 +1,12 @@
+function oddEven(number)
+{
+    if(number%2==0)
+    {
+        console.log(`number ${number} is a even no`);
+    }
+    else
+    console.log(`number ${number} is a odd no`);
+}
+
+
+oddEven(10);
